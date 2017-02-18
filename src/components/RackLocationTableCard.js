380ec6b94ae13b3266e8fa9d34d9339dc2da8ed9
lkpochincheng <d102752000@gmail.com>
2017-02-18 09:32:43 +0800

    change table to new package and fix styles

--- src/components/RackLocationTableCard.js
+++ src/components/RackLocationTableCard.js
@@ -1,71 +1,69 @@
 import React, { PropTypes } from 'react';
-import { Card, CardText } from 'material-ui/Card';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import { Table, Column, Cell } from 'fixed-data-table-2';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 
 import { doHighlightLocations } from '../actions';
 
+const fixDataTableHeight = document.documentElement.clientHeight * 0.7;
+const fixDataTableWidth = document.documentElement.clientWidth * 0.4;
+const fixDataTableColumnWidth = (fixDataTableWidth / 2);
 class RackLocationTableCard extends React.Component {
   render() {
     const { data } = this.props;
 
     return (
-      <Card>
-        <CardText>
-          <Table
-            height='450px'
-            fixedHeader={true}
-            selectable={false}
-            multiSelectable={false}
-            onRowHover={(i) => {
-              const { doHighlightLocations } = this.props;
-              doHighlightLocations({
-                highlightLocations: [data[i].rackLocation.trim()],
-                focusHighLightLocation: data[i].rackLocation.trim()
-              });
-            }}>
-            <TableHeader
-              displaySelectAll={false}
-              adjustForCheckbox={false}
-              enableSelectAll={false}>
-              <TableRow>
-                <TableHeaderColumn tooltip="Rack No.">Rack No.</TableHeaderColumn>
-                <TableHeaderColumn tooltip="Rack Location">Location</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody
-              displayRowCheckbox={false}
-              deselectOnClickaway={true}
-              showRowHover={true}
-              stripedRows={false}>
-              { data ? this.showData(data): ''}
-            </TableBody>
-          </Table>
-        </CardText>
-      </Card>
+      <div>
+        {data
+            ? <Table
+              rowHeight={50}
+              rowsCount={data.length}
+              width={fixDataTableWidth}
+              height={fixDataTableHeight}
+              headerHeight={50}
+              onRowMouseEnter={(i, j) => {
+                const { doHighlightLocations } = this.props;
+                doHighlightLocations({
+                  highlightLocations: [data[j].rackLocation.trim()],
+                  focusHighLightLocation: data[j].rackLocation.trim(),
+                });
+              }}
+            >
+              <Column
+                header={<Cell>rackName</Cell>}
+                cell={({ rowIndex, ...props }) => (
+                  <Cell {...props}>
+                    { data[rowIndex].rackName }
+                  </Cell>
+                    )}
+                width={fixDataTableColumnWidth}
+                align="center"
+              />
+              <Column
+                header={<Cell>rackLocation</Cell>}
+                cell={({ rowIndex, ...props }) => (
+                <Cell {...props}>
+                  { data[rowIndex].rackLocation }
+                </Cell>
+                )}
+                width={fixDataTableColumnWidth}
+                align="center"
+              />
+            </Table>
+        : '' }
+      </div>
     );
   }
-  showData(data) {
-    var rootDom = [];
-    _.map(data, function(i, k) {
-      rootDom.push(
-        <TableRow key={k} selected={false}>
-          <TableRowColumn>{i.rackName}</TableRowColumn>
-          <TableRowColumn>{i.rackLocation}</TableRowColumn>
-        </TableRow>);
-    });
-    return ( rootDom );
-  }
-};
+}
 
 RackLocationTableCard.propTypes = {
-  data: PropTypes.array
+  data: PropTypes.array,
+  doHighlightLocations: PropTypes.func,
 };
 
 const mapStateToProps = (state) => {
   return {
-    ...state.admin
+    ...state.admin,
   };
 };
 
@@ -73,5 +71,5 @@ export default connect(
   mapStateToProps,
   {
     doHighlightLocations,
-  }
+  },
 )(RackLocationTableCard);
